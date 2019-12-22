29e267c11f6db5605d421790cbfbe98933ba5133
zTrap (Peter Gulko) <ztrap.developer@gmail.com>
2019-12-23 01:52:57 +0300

    release [1.0.3]

--- validate-mapper/src/main/kotlin/ru/ztrap/tools/validate/checks/NotBlankStringCheck.kt
+++ validate-mapper/src/main/kotlin/ru/ztrap/tools/validate/checks/NotBlankStringCheck.kt
@@ -6,10 +6,10 @@ object NotBlankStringCheck : ValidateChecker {
 
     override fun invoke(raw: Any): Result {
         return if (raw is String) {
-            if (raw.isNotBlank()) {
-                Result.Success
-            } else {
-                Result.Error("blank string")
+            when {
+                raw.isEmpty() -> Result.Error("empty string")
+                raw.isBlank() -> Result.Error("blank string")
+                else -> Result.Success
             }
         } else {
             Result.Error("not a string")
